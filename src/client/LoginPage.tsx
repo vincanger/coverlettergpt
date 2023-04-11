38683086f8b1bf0d39080db26c3da07803c23d76
vincanger <70215737+vincanger@users.noreply.github.com>
2023-04-11 12:06:45 +0200

    auth

--- src/client/LoginPage.tsx
+++ src/client/LoginPage.tsx
@@ -1,23 +1,32 @@
 import useAuth from '@wasp/auth/useAuth';
 import { signInUrl } from '@wasp/auth/helpers/Google';
+
 import { AiOutlineGoogle } from 'react-icons/ai';
 import { VStack, Button, Spinner, Text } from '@chakra-ui/react';
 import BorderBox from './components/BorderBox';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Login() {
   const { data: user, isLoading, error } = useAuth();
 
+  const history = useHistory();
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [user, history]);
+
   return (
     <BorderBox>
-      {isLoading && <Spinner />}
       {error && <Text>Something went wrong :(</Text>}
-      {!user && (
-        <VStack>
+      {isLoading ? <Spinner /> 
+      : <VStack>
           <a href={signInUrl}>
             <Button leftIcon={<AiOutlineGoogle />}>Sign In</Button>
           </a>
         </VStack>
-      )}
+      }
     </BorderBox>
   );
 }
