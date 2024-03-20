1b30fad17a3ae9f0c569f74df9ad23e702e263b1
vincanger <70215737+vincanger@users.noreply.github.com>
2024-03-20 14:33:33 +0100

    update for wasp v0.13

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,31 +3,24 @@ import { z } from 'zod';
 
 const googleDataSchema = z.object({
   profile: z.object({
-    emails: z.array(
-      z.object({
-        value: z.string(),
-      })
-    ),
+    email: z.string(),
+    name: z.string(),
   }),
 });
 
 export const getUserFields = defineUserSignupFields({
-  email: (data) => {
+  email: (data: any) => {
     const googleData = googleDataSchema.parse(data);
-    return googleData.profile.emails[0].value;
+    return googleData.profile.email;
   },
-  username: (data) => {
+  username: (data: any) => {
     const googleData = googleDataSchema.parse(data);
-    return googleData.profile.emails[0].value.split('@')[0];
-  },
-});
+    return googleData.profile.name;
+  }
+})
 
 export function config() {
-  const clientID = process.env.GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
   return {
-    clientID, // look up from env or elsewhere,
-    clientSecret, // look up from env or elsewhere,
-    scope: ['profile', 'email'], // must include at least 'profile' for Google
+    scopes: ['profile', 'email'],
   };
 }
